e66a6d29662d56d7ce9715f20ad33c9cb8ed20b9
Jon Shaw <jon.shaw@jassoft.co.uk>
2019-03-21 14:54:33 +0000

    Refactor to work with new URL structure

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Running Locust benchmark"
-locust -f runner_benchmark/har_replay_test.py --host=http://localhost:5000 --no-web -c 1 -r 1 --csv=output -t 1m -L WARNING
+EQ_ID=${1} FORM_TYPE=${2} locust -f runner_benchmark/har_replay_test.py --host=http://localhost:5000 --no-web -c 1 -r 1 --csv=output -t 1m -L WARNING
 ./scripts/output_processor.py
