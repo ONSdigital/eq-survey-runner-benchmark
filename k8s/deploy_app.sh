f2d747eeec46427d94b76912d94412673e2c229f
Ian Wootten <hi@niftydigits.com>
2019-10-17 11:59:42 +0100

    Modify default settings from deploy script

--- k8s/deploy_app.sh
+++ k8s/deploy_app.sh
@@ -9,15 +9,11 @@ fi
 
 DOCKER_REGISTRY="${DOCKER_REGISTRY:-eu.gcr.io/census-eq-ci}"
 IMAGE_TAG="${IMAGE_TAG:-latest}"
-REQUESTS_JSON="${REQUESTS_JSON:-requests/census_household_gb_eng.json}"
-LOCUST_OPTS="${LOCUST_OPTS:-'-f locustfile.py --no-web -c 800 -r 10 --loglevel=CRITICAL --run-time=2h --only-summary'}"
 
 helm tiller run \
     helm upgrade --install \
     runner-benchmark \
     k8s/helm \
     --set host=${HOST} \
-    --set requestsJson=${REQUESTS_JSON} \
-    --set locustOptions=${LOCUST_OPTS} \
     --set image.repository=${DOCKER_REGISTRY}/eq-survey-runner-benchmark \
     --set image.tag=${IMAGE_TAG}
