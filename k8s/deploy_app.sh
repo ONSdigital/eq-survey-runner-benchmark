2800c513302f20e0bc790ac04fbc1df769616f47
Ian Wootten <hi@niftydigits.com>
2019-10-17 11:44:48 +0100

    Modify deploy_app defaults

--- k8s/deploy_app.sh
+++ k8s/deploy_app.sh
@@ -10,7 +10,7 @@ fi
 DOCKER_REGISTRY="${DOCKER_REGISTRY:-eu.gcr.io/census-eq-ci}"
 IMAGE_TAG="${IMAGE_TAG:-latest}"
 REQUESTS_JSON="${REQUESTS_JSON:-requests/census_household_gb_eng.json}"
-LOCUST_OPTS="${LOCUST_OPTS:--f locustfile.py --no-web -c 100 -r 10}"
+LOCUST_OPTS="${LOCUST_OPTS:-'-f locustfile.py --no-web -c 800 -r 10 --loglevel=CRITICAL --run-time=2h --only-summary'}"
 
 helm tiller run \
     helm upgrade --install \
