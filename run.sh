993592a6eeab850ca4043e95e164481b5233eb87
Andrew Maddaford <ajmaddaford@gmail.com>
2019-10-30 15:02:42 +0000

    Use url names to group results by endpoint (#10)

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 echo "Running Locust benchmark"
-REQUESTS_JSON="${1}" HOST="${2:-http://localhost:5000}" pipenv run locust --no-web -c 1 -r 1 -t 1m --csv=output -L WARNING
-./scripts/output_processor.py
+USER_WAIT_TIME_MIN_SECONDS=0 USER_WAIT_TIME_MAX_SECONDS=0 REQUESTS_JSON="${1}" HOST="${2:-http://localhost:5000}" pipenv run locust --no-web -c 1 -r 1 -t 1m --csv=output -L WARNING
