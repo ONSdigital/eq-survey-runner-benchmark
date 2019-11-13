ebd82b0bc1b1b558b1a84cffc6a2cf75dd2c91bf
Mebin Abraham <mebin.abraham@qa.com>
2019-11-13 11:28:00 +0000

    Address comments

--- store_benchmark_outputs.py
+++ store_benchmark_outputs.py
@@ -30,8 +30,6 @@ def get_runner_version(runner_url):
     if response.status_code == 200:
         return response.json().get('version', '').strip()
 
-    return None
-
 
 '''
 This script allows the upload of the benchmark outputs to a GCS Bucket.
@@ -39,31 +37,32 @@ If running locally, you must specify valid Application Default Credentials (ADC)
 https://cloud.google.com/docs/authentication/production
 '''
 if __name__ == '__main__':
-    gcs_bucket_name = os.getenv('GCS_OUTPUT_BUCKET')
-    if gcs_bucket_name:
-        host = os.getenv('HOST')
-        locust_options = os.getenv('LOCUST_OPTS')
-        requests_json = os.getenv('REQUESTS_JSON')
-        output_directory = os.getenv('OUTPUT_DIRECTORY')
-        min_wait = os.getenv('USER_WAIT_TIME_MIN_SECONDS')
-        max_wait = os.getenv('USER_WAIT_TIME_MAX_SECONDS')
-        filename_prefix = os.getenv('OUTPUT_FILENAME_PREFIX')
-        runner_version = get_runner_version(host)
-        try:
-            gcs = GoogleCloudStorage(bucket_name=gcs_bucket_name)
-            gcs.upload_files(
-                output_files=('output_requests.csv', 'output_distribution.csv'),
-                directory=output_directory,
-                output_filename_prefix=filename_prefix,
-                host=host,
-                min_wait=min_wait,
-                max_wait=max_wait,
-                requests_json=requests_json,
-                locust_options=locust_options,
-                runner_version=runner_version,
-                timestamp=int(datetime.utcnow().timestamp()),
-            )
-        except Exception as ex:
-            raise Exception(f"Error occurred during file upload to GCS - {ex}")
-    else:
-        raise Exception('GCS Bucket not specified by `GCS_OUTPUT_BUCKET`')
+    gcs_bucket_name = os.environ['GCS_OUTPUT_BUCKET']
+
+    if not gcs_bucket_name:
+        raise ValueError(
+            'Value for `GCS_OUTPUT_BUCKET` environment variable not valid.'
+        )
+
+    host = os.getenv('HOST')
+    runner_version = get_runner_version(host)
+    locust_options = os.getenv('LOCUST_OPTS')
+    requests_json = os.getenv('REQUESTS_JSON')
+    output_directory = os.getenv('OUTPUT_DIRECTORY')
+    min_wait = os.getenv('USER_WAIT_TIME_MIN_SECONDS')
+    max_wait = os.getenv('USER_WAIT_TIME_MAX_SECONDS')
+    filename_prefix = os.getenv('OUTPUT_FILENAME_PREFIX')
+
+    gcs = GoogleCloudStorage(bucket_name=gcs_bucket_name)
+    gcs.upload_files(
+        output_files=('output_requests.csv', 'output_distribution.csv'),
+        directory=output_directory,
+        output_filename_prefix=filename_prefix,
+        host=host,
+        min_wait=min_wait,
+        max_wait=max_wait,
+        requests_json=requests_json,
+        locust_options=locust_options,
+        runner_version=runner_version,
+        timestamp=int(datetime.utcnow().timestamp()),
+    )
