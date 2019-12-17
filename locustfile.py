697f6bf184c62a4e60944c42e729c600729dcc8e
James Gardiner <james@jgardiner.co.uk>
2019-12-17 13:01:32 +0000

    Switch out HttpLocust for FastHttpLocust

--- locustfile.py
+++ locustfile.py
@@ -1,10 +1,10 @@
 import os
 
-from locust import HttpLocust
+from locust import FastHttpLocust
 
 from runner_benchmark.taskset import SurveyRunnerTaskSet
 
 
-class SurveyRunnerScenario(HttpLocust):
+class SurveyRunnerScenario(FastHttpLocust):
     task_set = SurveyRunnerTaskSet
     host = os.getenv("HOST", "http://localhost:5000")
