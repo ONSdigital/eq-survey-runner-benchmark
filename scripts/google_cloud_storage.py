e99be484c4bc337a64f3c5dc6ce37ba396d25f51
Rhys Berrow <rhys.berrow@ons.gov.uk>
2020-01-31 15:01:58 +0000

    PR comments

--- scripts/google_cloud_storage.py
+++ scripts/google_cloud_storage.py
@@ -25,17 +25,19 @@ class GoogleCloudStorage:
             blob.upload_from_filename(filename=output_file)
 
     def get_files(self, output_dir):
+
         for blob in self.client.list_blobs(self.bucket_name):
 
             file_path = '/'.join(iter(blob.name.split('/')[:2]))
+
             output_file_path = f'{output_dir}/{file_path}'
+
             if not os.path.exists(output_file_path):
                 os.makedirs(output_file_path)
 
             blob.download_to_filename(filename=f'{output_dir}/{blob.name}')
 
-            metadata_file = (
-                f"{output_dir}/{file_path}/metadata.json"
-            )
-            with open(metadata_file, 'w+') as f:
-                f.write(json.dumps(blob.metadata, indent=2))
+            metadata_file_path = f"{output_dir}/{file_path}/metadata.json"
+            if not os.path.exists(metadata_file_path):
+                with open(metadata_file_path, 'w+') as file:
+                    file.write(json.dumps(blob.metadata, indent=2))
