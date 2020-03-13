124f0fc36862ae59e61e9e004861669361f69ac5
Andrew Maddaford <ajmaddaford@gmail.com>
2020-03-13 15:23:44 +0000

    Better error handling for 200 and 302 responses (#23)

--- runner_benchmark/questionnaire_mixins.py
+++ runner_benchmark/questionnaire_mixins.py
@@ -5,31 +5,48 @@ class QuestionnaireMixins:
     client = None
     csrf_token = None
 
-    def get(self, url, allow_redirects=False, name=None):
-        response = self.client.get(allow_redirects=allow_redirects, url=url, name=name)
+    def get(self, url, name=None, expect_redirect=False):
+        with self.client.get(url=url, name=name, allow_redirects=False, catch_response=True) as response:
+            
+            if expect_redirect:
+                if response.status_code != 302:
+                    error = f"Expected a (302) but got a ({response.status_code}) back when getting page: {url}"
+                    response.failure(error)
+                    raise Exception(error)
+                return response
 
-        if not response.content:
-            raise Exception(f"No content in GET response for url: {url}")
+            if response.status_code != 200:
+                error = f"Expected a (200) but got a ({response.status_code}) back when getting page: {url}"
+                response.failure(error)
+                raise Exception(error)
 
-        if response.status_code != 302:
+            if not response.content:
+                response.failure(f"No content in GET response for url: {url}")
+
             self.csrf_token = _extract_csrf_token(response.content.decode('utf8'))
 
-        return response
+            return response
 
     def post(self, base_url, request_url, data={}, name=None):
 
         data['csrf_token'] = self.csrf_token
-
         headers = {'Referer': base_url}
 
-        response = self.client.post(
+        with self.client.post(
             allow_redirects=False,
             headers=headers,
             data=data,
             url=request_url,
             name=name,
-        )
-        return response
+            catch_response=True,
+        ) as response:
+
+            if response.status_code != 302:
+                error = f"Expected a (302) but got a ({response.status_code}) back when posting page: {request_url} with data: {data}"
+                response.failure(error)
+                raise Exception(error)
+
+            return response
 
 
 CSRF_REGEX = re.compile(
