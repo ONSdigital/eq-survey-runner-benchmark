ecdd19570fd71f69a2029b3503b6082a0ccd4ef2
James Gardiner <james@jgardiner.co.uk>
2020-07-30 13:28:13 +0100

    Add the module needed

---
+++ scripts/benchmark_stats.py
@@ -0,0 +1,60 @@
+from csv import DictReader
+from glob import glob
+from statistics import mean
+from typing import Generator, Iterable, List
+
+
+class BenchmarkStats:
+    def __init__(self, folder_path: str):
+        self._files: List[str] = glob(folder_path + "/*stats.csv")
+        self.get_requests: List[int] = []
+        self.post_requests: List[int] = []
+        self.total_requests: int = 0
+        self.total_failures: int = 0
+        self.error_percentage: int
+        self._data = self._process_file_data()
+
+    def __str__(self):
+        return (
+            f'Questionnaire GETs average: {int(self.average_get)}ms\n'
+            f'Questionnaire POSTs average: {int(self.average_post)}ms\n'
+            f'All requests average: {int(self.average_total)}ms\n'
+            f'Total Requests: {int(self.total_requests):,}\n'
+            f'Total Failures: {int(self.total_failures):,}\n'
+            f'Error Percentage: {(round(self.error_percentage, 2))}%\n'
+        )
+
+    def _process_file_data(self):
+        for file in self.files:
+            with open(file) as fp:
+                for row in DictReader(fp, delimiter=","):
+                    percentile_99th = int(row["99%"])
+                    if "/questionnaire" in row['Name']:
+                        if row["Type"] == "GET":
+                            self.get_requests.append(percentile_99th)
+                        elif row["Type"] == "POST":
+                            self.post_requests.append(percentile_99th)
+
+                    if row["Name"] == "Aggregated":
+                        self.total_requests += int(row["Request Count"])
+                        self.total_failures += int(row["Failure Count"])
+
+    @property
+    def files(self) -> Generator[str, None, None]:
+        return self._files
+
+    @property
+    def average_get(self):
+        return mean(self.get_requests)
+
+    @property
+    def average_post(self):
+        return mean(self.post_requests)
+
+    @property
+    def average_total(self):
+        return mean(self.get_requests + self.post_requests)
+
+    @property
+    def error_percentage(self):
+        return (self.total_failures * 100) / self.total_requests
