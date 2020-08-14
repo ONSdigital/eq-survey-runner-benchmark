9970a37b5e87d747d03a302554d6f02290cf0035
LJBabbage <lewis.babbage@ons.gov.uk>
2020-08-14 08:02:45 +0100

    Benchmark to include request weighting (#54)
    * Update Benchmark to include request weighting and restore 99.9 percentile

--- scripts/benchmark_stats.py
+++ scripts/benchmark_stats.py
@@ -1,6 +1,5 @@
 from csv import DictReader
 from glob import glob
-from statistics import mean
 from typing import List
 
 
@@ -10,17 +9,18 @@ class BenchmarkStats:
         for folder_path in folder_paths:
             self._files.extend(glob(f"{folder_path}/*stats.csv"))
 
-        self.get_requests: List[int] = []
-        self.post_requests: List[int] = []
-        self.total_requests: int = 0
+        self.weighted_get_requests: List[int] = []
+        self.weighted_post_requests: List[int] = []
+        self.total_get_requests: int = 0
+        self.total_post_requests: int = 0
         self.total_failures: int = 0
         self._process_file_data()
 
     def __str__(self):
         return (
-            f'Questionnaire GETs average: {int(self.average_get)}ms\n'
-            f'Questionnaire POSTs average: {int(self.average_post)}ms\n'
-            f'All requests average: {int(self.average_total)}ms\n'
+            f'Questionnaire GETs average: {int(self.average_weighted_get)}ms\n'
+            f'Questionnaire POSTs average: {int(self.average_weighted_post)}ms\n'
+            f'All requests average: {int(self.average_weighted_total)}ms\n'
             f'Total Requests: {int(self.total_requests):,}\n'
             f'Total Failures: {int(self.total_failures):,}\n'
             f'Error Percentage: {(round(self.error_percentage, 2))}%\n'
@@ -31,16 +31,18 @@ class BenchmarkStats:
             with open(file) as fp:
                 for row in DictReader(fp, delimiter=","):
                     percentile_99th = int(row["99%"])
+                    request_count = int(row.get("Request Count") or row.get("# requests"))
+
                     if "/questionnaire" in row['Name']:
                         if row["Type"] == "GET":
-                            self.get_requests.append(percentile_99th)
+                            self.weighted_get_requests.append(percentile_99th * request_count)
+                            self.total_get_requests += request_count
                         elif row["Type"] == "POST":
-                            self.post_requests.append(percentile_99th)
+                            self.weighted_post_requests.append(percentile_99th * request_count)
+                            self.total_post_requests += request_count
 
                     if row["Name"] == "Aggregated":
-                        request_count = row.get("Request Count") or row.get("# requests")
                         failure_count = row.get("Failure Count") or row.get("# failures")
-                        self.total_requests += int(request_count)
                         self.total_failures += int(failure_count)
 
     @property
@@ -48,17 +50,21 @@ class BenchmarkStats:
         return self._files
 
     @property
-    def average_get(self):
-        return mean(self.get_requests)
+    def average_weighted_get(self):
+        return sum(self.weighted_get_requests) / self.total_get_requests
 
     @property
-    def average_post(self):
-        return mean(self.post_requests)
+    def average_weighted_post(self):
+        return sum(self.weighted_post_requests) / self.total_post_requests
 
     @property
-    def average_total(self):
-        return mean(self.get_requests + self.post_requests)
+    def average_weighted_total(self):
+        return sum(self.weighted_get_requests + self.weighted_post_requests) / self.total_post_requests
 
     @property
     def error_percentage(self):
         return (self.total_failures * 100) / self.total_requests
+
+    @property
+    def total_requests(self):
+        return self.total_get_requests + self.total_post_requests
