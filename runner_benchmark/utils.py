78f971b6a0694013c30d6c2ab21b30fd1b81ea45
Rhys Berrow <rhys.berrow@ons.gov.uk>
2020-12-23 09:13:11 +0000

    Allow redirect params with query values

--- runner_benchmark/utils.py
+++ runner_benchmark/utils.py
@@ -7,15 +7,21 @@ def parse_params_from_location(url, route):
 
     params = {}
 
-    fields = [
+    path_fields = [
         (i, element[1:-1])
         for i, element in enumerate(route_path.split("/"))
         if element.startswith('{') and element.endswith('}')
     ]
 
     for value_pos, value in enumerate(url_path.split("/")):
-        for field_pos, field_name in fields:
+        for field_pos, field_name in path_fields:
             if field_pos == value_pos:
                 params[field_name] = value
+
+    if urlparse(route).query:
+        query_result = urlparse(route).query.partition("=")[2]
+        url_query = urlparse(url).query.partition("=")[2]
+
+        params[query_result.strip("{}")] = url_query
 
     return params
