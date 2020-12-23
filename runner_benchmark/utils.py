6f6dda2cda067073861bc9cee50b508e15f85136
Rhys Berrow <rhys.berrow@ons.gov.uk>
2020-12-23 11:50:56 +0000

    Revert unneeded change

--- runner_benchmark/utils.py
+++ runner_benchmark/utils.py
@@ -7,14 +7,14 @@ def parse_params_from_location(url, route):
 
     params = {}
 
-    path_fields = [
+    fields = [
         (i, element[1:-1])
         for i, element in enumerate(route_path.split("/"))
         if element.startswith('{') and element.endswith('}')
     ]
 
     for value_pos, value in enumerate(url_path.split("/")):
-        for field_pos, field_name in path_fields:
+        for field_pos, field_name in fields:
             if field_pos == value_pos:
                 params[field_name] = value
 
