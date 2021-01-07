ad5f4d658247092b3c46d142b8cc47b0f49641d1
Rhys Berrow <rhys.berrow@ons.gov.uk>
2021-01-07 10:41:32 +0000

    Simplify logic

--- runner_benchmark/utils.py
+++ runner_benchmark/utils.py
@@ -2,29 +2,26 @@ from urllib.parse import urlparse, parse_qs
 
 
 def parse_params_from_location(url, route):
-    route_path = urlparse(route).path
-    url_path = urlparse(url).path
+    parsed_route = urlparse(route)
+    parsed_url = urlparse(url)
 
     params = {}
 
     fields = [
         (i, element[1:-1])
-        for i, element in enumerate(route_path.split("/"))
+        for i, element in enumerate(parsed_route.path.split("/"))
         if element.startswith('{') and element.endswith('}')
     ]
 
-    for value_pos, value in enumerate(url_path.split("/")):
+    for value_pos, value in enumerate(parsed_url.path.split("/")):
         for field_pos, field_name in fields:
             if field_pos == value_pos:
                 params[field_name] = value
 
-    if urlparse(route).query:
-        query_result = parse_qs(urlparse(route).query)
-        url_result = parse_qs(urlparse(url).query)
+    if parsed_route.query:
+        query_result = parse_qs(parsed_route.query)
+        url_result = parse_qs(parsed_url.query)
 
-        query_result.update((key, url_result[key]) for key in query_result)
-
-        for key, value in query_result.items():
-            params[key] = value[0]
+        params.update((key, url_result[key][0]) for key in query_result)
 
     return params
