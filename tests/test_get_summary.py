24e09a3860cfb60b46306a1d5fb6bcf0cf880537
petechd <petechadwick1944@gmail.com>
2024-04-24 10:59:40 +0100

    Fix linting and tests

--- tests/test_get_summary.py
+++ tests/test_get_summary.py
@@ -62,6 +62,7 @@ def test_parse_environment_variables_with_valid_number_of_days(monkeypatch):
     monkeypatch.setenv("NUMBER_OF_DAYS", "10")
     monkeypatch.setenv("OUTPUT_DATE", "2024-02-07")
     monkeypatch.setenv("OUTPUT_DIR", "outputs")
+    monkeypatch.setenv("OUTPUT_TO_GITHUB", "False")
 
     env_vars = parse_environment_variables()
 
@@ -69,6 +70,7 @@ def test_parse_environment_variables_with_valid_number_of_days(monkeypatch):
         "number_of_days": 10,
         "output_date": "2024-02-07",
         "output_dir": "outputs",
+        "output_to_github": "False",
     }
 
 
