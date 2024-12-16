cac89334e1942418fab369973f32e3732509799f
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2024-12-16 13:03:36 +0000

    update tests and add new tests

--- tests/test_slack_notification.py
+++ tests/test_slack_notification.py
@@ -1,6 +1,7 @@
 import pytest
 
 from scripts.slack_notification import (
+    get_channel_id,
     parse_environment_variables,
     post_slack_notification,
 )
@@ -68,6 +69,17 @@ def test_parse_environment_variables_attachment_filename_not_valid(monkeypatch):
 
 
 def test_post_slack_notification_with_ok_response_raises_no_error(mocker):
+    mock_conversations_list = mocker.patch(
+        "slack_sdk.web.client.WebClient.conversations_list",
+        side_effect=[
+            {
+                "ok": True,
+                "channels": [{"id": "C12345", "name": "test-alerts"}],
+                "response_metadata": {"next_cursor": ""},
+            }
+        ],
+    )
+
     mocker.patch(
         "slack_sdk.web.client.WebClient.files_upload_v2", return_value={"ok": True}
     )
@@ -86,6 +98,17 @@ def test_post_slack_notification_with_ok_response_raises_no_error(mocker):
 def test_post_slack_notification_with_no_content_and_ok_response_raises_no_error(
     mocker,
 ):
+    mock_conversations_list = mocker.patch(
+        "slack_sdk.web.client.WebClient.conversations_list",
+        side_effect=[
+            {
+                "ok": True,
+                "channels": [{"id": "C12345", "name": "test-alerts"}],
+                "response_metadata": {"next_cursor": ""},
+            }
+        ],
+    )
+
     mocker.patch(
         "slack_sdk.web.client.WebClient.files_upload_v2", return_value={"ok": True}
     )
@@ -129,3 +152,47 @@ def test_post_slack_notification_with_api_error_exits():
             initial_comment="comment",
             attachment_filename="file",
         )
+
+
+def test_get_channel_id_with_ok_response_raises_no_error(mocker):
+    mock_response = {
+        "ok": True,
+        "channels": [
+            {"id": "C12345678", "name": "test-alert"},
+            {"id": "C87654321", "name": "general"},
+        ],
+    }
+
+    mock_client = mocker.Mock()
+
+    mock_client.conversations_list.return_value = mock_response
+
+    channel_id = get_channel_id(mock_client, "test-alert")
+
+    assert channel_id == "C12345678"
+
+
+def test_get_channel_id_channel_not_found(mocker):
+    mock_response = {
+        "ok": True,
+        "channels": [
+            {"id": "C12345678", "name": "test-alert"},
+            {"id": "C87654321", "name": "general"},
+        ],
+    }
+
+    mock_client = mocker.Mock()
+    mock_client.conversations_list.return_value = mock_response
+
+    with pytest.raises(SystemExit):
+        get_channel_id(mock_client, "random-channel")
+
+
+def test_get_channel_id_fails_to_fetch_channels(mocker):
+    mock_response = {"ok": False, "error": "invalid_auth"}
+
+    mock_client = mocker.Mock()
+    mock_client.conversations_list.return_value = mock_response
+
+    with pytest.raises(SystemExit):
+        get_channel_id(mock_client, "test-alert")
